f06e8d2ba1f919a6bf8fb91e40d7aa71a1987026
SUMINSON01 <56192209+SUMINSON01@users.noreply.github.com>
2021-09-07 19:22:21 +0900

    Update divisor.py

--- Level1/divisor.py
+++ Level1/divisor.py
@@ -9,3 +9,16 @@ def divisor(num): # 약수 갯수 구하기
         return True
     else:
         return False
+
+def solution(left, right):
+    answer = 0
+    for i in range(left, right+1):
+        if divisor(i):
+            answer += i
+        else:
+            answer -= i
+        #if int(sqrt) == sqrt: # 제곱근이 정수일 경우 약수가 홀수
+        #    answer -= i
+        #else:
+        #    answer += i
+    return answer
